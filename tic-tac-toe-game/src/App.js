25b81f2665b739d456068d681a5782a365b9bbf7
myfAirCS <younasf69@gmail.com>
2024-12-21 18:08:17 +0500

    creating tictactoe game . learned use of fragments " <> </> "

---
+++ tic-tac-toe-game/src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+<div>
+  <button>
+    1
+  </button>
+
+  <button>
+    2
+  </button>
+
+  <button>
+    3
+  </button>
+</div>
+
+<div>
+  <button>
+    4
+  </button>
+
+  <button>
+    5
+  </button>
+
+  <button>
+    6
+  </button>
+</div>
+
+<div>
+  <button>
+    7
+  </button>
+
+  <button>
+    8
+  </button>
+
+  <button>
+    9
+  </button>
+</div>
+      </>
+  );
+}
+
+export default App;
