ae1fee35d7c7d803cbf9dd240f0f62f4eba1f2e1
myfAirCS <younasf69@gmail.com>
2024-12-28 14:40:08 +0500

    Learned Props and How to configure tailwind in REACT

---
+++ TailwindAndProps/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from './components/Card'
+import './App.css'
+
+function App() {
+  return(
+    <>
+
+<Card title="Sunset" imageUrl='https://images.pexels.com/photos/1212600/pexels-photo-1212600.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' description="Calm Blue Sea During Golden Hour"  />
+</>
+    )
+}
+
+export default App
---
+++ TailwindAndProps/src/components/Card.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+//props are Attributes of the JSX compnent functions we either pass props to the function and use its attributes
+// or directly destructure them and use them in the component. 
+// it is basically just like the way we use res and req in API
+export default function Card(props,{description}) {
+  return (
+    
+<div className="max-w-sm rounded overflow-hidden shadow-lg">
+  <img className="w-full" src={props.imageUrl} alt="Sunset in the mountains"/>
+  <div className="px-6 py-4">
+    <div className="font-bold text-xl mb-2">{props.title}</div>
+    <p className="text-gray-700 text-base">
+    {description}
+    </p>
+  </div>
+  <div className="px-6 pt-4 pb-2">
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
+
+  );
+}
