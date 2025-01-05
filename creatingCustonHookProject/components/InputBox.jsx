0050fb694c3537c57812c58f77ed116f55382447
“myfAirCS” <younasf69@gmail.com>
2025-01-05 19:31:55 +0500

    learned useId hook : it should not be used as a key

--- creatingCustonHookProject/components/InputBox.jsx
+++ creatingCustonHookProject/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useId } from 'react'
 
 function InputBox({
   label,
@@ -12,15 +12,15 @@ function InputBox({
   currencyDisable=false
 }) {
  
-
+const amountId=useId();
   return (
       <div className={`bg-white p-3 rounded-lg text-sm flex ${className} `}>
           <div className="w-1/2">
-              <label  className="text-black/40 mb-2 inline-block">
+              <label htmlFor={amountId}  className="text-black/40 mb-2 inline-block">
                   {label}
               </label>
               <input
-                  
+                  id={amountId}
                   className="outline-none w-full bg-transparent py-1.5"
                   type="number"
                   placeholder="Amount"
@@ -33,6 +33,7 @@ function InputBox({
           <div className="w-1/2 flex flex-wrap justify-end text-right">
               <p className="text-black/40 mb-2 w-full">Currency Type</p>
               <select
+                    disabled={currencyDisable}
                   className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
                   value={currency}
                   onChange={(e)=>onCurrencyChange && onCurrencyChange(e.target.value)} 
