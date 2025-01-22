bf87acc7557922253d1b003000120497e585d8a3
“myfAirCS” <younasf69@gmail.com>
2025-01-23 00:21:03 +0500

    Created a ThemeSwitcher In REACT JS using Context API

--- ThemeSwitcher/src/App.jsx
+++ ThemeSwitcher/src/App.jsx
@@ -1,15 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/Theme'
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
 
 function App() {
-  const [count, setCount] = useState(0)
+const [themeMode,setThemeMode]=useState("light");
+const lightTheme=()=>{
+  setThemeMode("light");
+};
+const darkTheme=()=>{
+  setThemeMode("dark");
+};
+
+useEffect(()=>{
+const HTML=document.querySelector("html");
+HTML.classList.remove("dark","light");
+HTML.classList.add(themeMode);
+},[themeMode])
 
   return (
-    <>
-      
-    </>
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        <ThemeBtn/>
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       <Card/>
+                    </div>
+                </div>
+            </div>
+            </ThemeProvider>
   )
 }
 
