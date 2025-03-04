46fd0f076aa71f851895a3a5bde3102fd67c1e29
“myfAirCS” <younasf69@gmail.com>
2025-03-05 00:06:46 +0500

    Created Infinite Scroll

--- infiniteScroll/src/components/common/post/Post.jsx
+++ infiniteScroll/src/components/common/post/Post.jsx
@@ -4,7 +4,7 @@ const Post = ({ data }) => {
   if (!data) return <p className="text-center text-gray-500">Loading...</p>;
 
   return (
-    <div className="max-w-lg mx-auto p-6 bg-white rounded-2xl shadow-lg border border-gray-200 flex items-center space-x-4  mt-8">
+    <div className="max-w-lg mx-auto p-6 bg-white rounded-2xl shadow-lg border border-gray-200 flex items-center space-x-4 mb-4">
       <img src={data.image} alt={data.name} className="w-16 h-16 rounded-full border border-gray-300" />
       <div>
         <h2 className="text-xl font-bold text-gray-900">{data.name}</h2>
